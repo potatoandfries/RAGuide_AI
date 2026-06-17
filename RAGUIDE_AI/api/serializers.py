29e9db545f6084bbf39690dad28492e145f75e3d
--show-origin <--show-origin>
2026-06-18 01:45:31 +0800

    Add serializers for PDFDocument and DocumentChunk

---
+++ RAGUIDE_AI/api/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import PDFDocument, DocumentChunk
+# A serializer is:
+
+# A DTO(data to object) + validation + conversion logic
+
+class PDFDocumentSerializer(serializers.ModelSerializer):
+    class Meta: #Settings for Serializer
+        model = PDFDocument
+        fields = ['id', 'user', 'title', 'file', 'uploaded_at', 'chunk_size']
+
+class DocumentChunkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DocumentChunk
+        fields = ['id', 'document', 'content', 'chunk_index', 'embedding']
